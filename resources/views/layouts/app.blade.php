af6fe07ce6a865348d03cab04d04646c338f06f9
samad <samadha56@gmail.com>
2023-02-04 23:19:06 +0330

    add samim font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,15 +17,35 @@
         integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        @font-face {
+            font-family: Samim;
+            src: url({{ asset('fonts/samim/Samim.ttf') }}) format('truetype');
+            font-weight: normal;
+        }
+
+        @font-face {
+            font-family: Samim;
+            src: url({{ asset('fonts/samim/Samim-Bold.ttf') }}) format('truetype');
+            font-weight: bold;
+        }
+
+        @font-face {
+            font-family: Samim;
+            src: url({{ asset('fonts/samim/Samim-Medium.ttf') }}) format('truetype');
+            font-weight: 500;
+        }
+
+        body {
+            font-family: 'Samim', sans-serif;
+        }
+    </style>
 </head>
 
 <body>
