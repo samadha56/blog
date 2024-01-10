ed72bd177008f31e387d0639fe76423c079d1362
samad <samad@samads-MacBook-Air.local>
2024-01-10 23:57:14 +0330

    enhance docker file

--- docker-start.bash
+++ docker-start.bash
@@ -11,5 +11,11 @@ echo "starting sleep ..."
 sleep 10
 
 echo "starting laravel commands ..."
-docker compose run --rm artisan migrate
-docker compose run --rm artisan db:seed
+docker-compose run --rm artisan migrate
+docker-compose run --rm artisan db:seed
+
+# if need permission
+# docker exec -it php sh
+# chmod -R 775 /var/www/html/storage
+# chmod -R 775 /var/www/html/bootstrap/cache
+# chown -R laravel:laravel /var/www/html
