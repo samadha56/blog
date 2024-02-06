fde0d6657abf04b28b0d64d32d0312339fc810f9
samad <samad@samads-MacBook-Air.local>
2024-02-06 21:52:22 +0330

    show short content in post blade file

--- resources/views/site/post.blade.php
+++ resources/views/site/post.blade.php
@@ -4,6 +4,7 @@
     <h1 class="text-center" style="font-size: 22px;" dir="rtl">{{ $post->title }}</h1>
     <h6 class="text-center"><small>{{ date('Y-m-d', strtotime($post->created_at)) }}</small></h6>
     <hr>
+    <div>{!! $post->short_content !!}</div>
     <div>{!! $post->content !!}</div>
 </div>
 @endsection
